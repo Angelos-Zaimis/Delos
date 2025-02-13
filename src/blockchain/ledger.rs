01d4c78141ad231016dca278c0149bfe5c38f78e
AngelosZa <aggeloszaimiis@gmail.com>
2025-02-13 18:55:22 +0100

    feat: transactions and storage

--- src/blockchain/ledger.rs
+++ src/blockchain/ledger.rs
@@ -1,37 +1,79 @@
+use rocksdb::{Options, DB};
 use super::transaction::Transaction;
 use crate::blockchain::Block;
 
 const BLOCK_TARGET_TIME: u64 = 10;
 const ADJUSTMENT_BLOCK_COUNT: usize = 5;
+const MIN_TRANSACTIONS_FOR_BLOCK: usize = 2;
 
 #[derive(Debug)]
 pub struct Ledger {
     pub chain: Vec<Block>,
-    pub transactions: Vec<Transaction>,
+    pub mempool: Vec<Transaction>,
     pub difficulty: u32,
+    pub db: DB,
+    pub total_fees_collected: f64,
 }
 
 impl Ledger {
     pub fn new() -> Self {
-        let genesis_block = Block::new(0, String::from("0"), String::from("Genesis block"), 2);
+        let mut opts = Options::default();
+        opts.create_if_missing(true);
+        let db = DB::open(&opts, "data/blockchain_db").expect("Failed to open RocksDB");
+
+        let mut chain = Vec::new();
+        let mut mempool = Vec::new();
+
+        // Load existing blockchain from RocksDB
+        if let Ok(blockchain_data) = db.get("blockchain") {
+            if let Some(data) = blockchain_data {
+                chain = serde_json::from_slice(&data).unwrap_or_else(|_| vec![]);
+            }
+        }
+
+        // Load existing mempool from RocksDB
+        if let Ok(mempool_data) = db.get("mempool") {
+            if let Some(data) = mempool_data {
+                mempool = serde_json::from_slice(&data).unwrap_or_else(|_| vec![]);
+            }
+        }
+
+        if chain.is_empty() {
+            // If the database is empty, create a genesis block
+            let genesis_block = Block::new(0, String::from("0"), String::from("Genesis block"), 2);
+            chain.push(genesis_block);
+        }
+
         Self {
-            chain: vec![genesis_block],
-            transactions: Vec::new(),
-            difficulty: 2
+            chain,
+            mempool,
+            difficulty: 2,
+            db,
+            total_fees_collected: 0.0,
         }
     }
 
     pub fn add_transaction(&mut self, transaction: Transaction) {
         if transaction.is_valid() {
-            self.transactions.push(transaction);
+            self.mempool.push(transaction);
+            self.save_mempool();
         } else {
             println!("Invalid transaction: {:?}", transaction);
         }
     }
 
     pub fn mine_block(&mut self) {
-        let last_block: &Block = self.chain.last().unwrap();
-        let data: String = format!("{:?}", self.transactions);
+        if self.mempool.len() < MIN_TRANSACTIONS_FOR_BLOCK {
+            println!("Not enough transactions in the mempool to mine a block. Waiting...");
+            return;
+        }
+
+        let selected_transactions = self.select_transactions_for_block();
+        let last_block = self.chain.last().unwrap();
+        let total_fees: f64 = selected_transactions.iter().map(|tx| tx.fee).sum();
+        self.total_fees_collected += total_fees;
+
+        let data: String = format!("{:?}", selected_transactions);
         let mut new_block = Block::new(last_block.index + 1, last_block.hash.clone(), data, self.difficulty);
 
         let target_prefix = "0".repeat(self.difficulty as usize);
@@ -53,9 +95,36 @@ impl Ledger {
         println!("Block Mined! Nonce: {}, Hash: {}, Difficulty: {}, Time: {} sec", new_block.nonce, new_block.hash, self.difficulty, mining_time);
 
         self.chain.push(new_block);
-        self.transactions.clear();
+        self.save_chain();
+        self.save_mempool();
+        self.save_total_fees();
+
+        self.mempool.clear();
 
         self.adjust_difficulty();
+
+    }
+
+    fn save_total_fees(&self) {
+        let serialized_fees = serde_json::to_string(&self.total_fees_collected).unwrap();
+        self.db.put("total_fees", serialized_fees).expect("Failed to save total fees");
+    }
+
+    fn save_chain(&self) {
+        let serialized_chain = serde_json::to_string(&self.chain).unwrap();
+        self.db.put("blockchain", serialized_chain).unwrap()
+    }
+
+    fn save_mempool(&self) {
+        let serialized_mempool = serde_json::to_string(&self.chain).unwrap();
+        self.db.put("mempool", serialized_mempool).expect("Failed to save mempool");
+    }
+
+    fn select_transactions_for_block(&mut self) -> Vec<Transaction> {
+        self.mempool.sort_by(|a,b| b.fee.partial_cmp(&a.fee).unwrap());
+
+        let selected = self.mempool.iter().take(MIN_TRANSACTIONS_FOR_BLOCK).cloned().collect();
+        selected
     }
 
     fn adjust_difficulty(&mut self) {
--- src/network/sync.rs
+++ src/network/sync.rs
@@ -1,7 +1,4 @@
-use tokio::net::TcpStream;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use bincode;
-use serde::{Deserialize};
 use crate::blockchain::{Block, Blockchain};
 use crate::network::peers::PeerManager;
 
@@ -19,36 +16,6 @@ impl Synchronizer {
     }
 
     pub async fn synchronize(&mut self) {
-        for peer in self.peer_manager.get_peers() {
-            match TcpStream::connect(&peer.address).await {
-                Ok(mut stream) => {
-                    println!("Connected to peer: {}", peer.address);
-
-                    if let Err(e) = stream.write_all(b"get_latest_block").await {
-                        println!("Failed to send request to {}: {}", peer.address, e);
-                        continue;
-                    }
-
-                    let mut buffer = vec![0; 1024];
-                    match stream.read(&mut buffer).await {
-                        Ok(n) if n > 0 => {
-                            if let Some(latest_block) = self.deserialize_block(&buffer[..n]) {
-                                if self.synchronize(latest_block) {
-                                    println!("Block added to the blockchain!");
-                                } else {
-                                    println!("Received invalid block from {}", peer.address);
-                                }
-                            } else {
-                                println!("Failed to deserialize block from {}", peer.address);
-                            }
-                        }
-                        Ok(_) => println!("No data received from {}", peer.address),
-                        Err(e) => println!("Failed to read from {}: {}", peer.address, e),
-                    }
-                }
-                Err(e) => println!("Failed to connect to {}: {}", peer.address, e),
-            }
-        }
 
         fn deserialize_block(data: &[u8]) -> Option<Block> {
             match bincode::deserialize::<Block>(data) {
